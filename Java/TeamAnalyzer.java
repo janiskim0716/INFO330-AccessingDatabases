7d40efe4a3dc39020eb27b1eaa154eef79a8f682
Janis Kim <janisk2@uw.edu>
2023-05-04 15:08:59 -0700

    finished

--- Java/TeamAnalyzer.java
+++ Java/TeamAnalyzer.java
@@ -1,62 +1,61 @@
-import java.sql.*;
+# imports:
+import sqlite3  # This is the package for all sqlite3 access in Python
+import sys      # This helps with command-line parameters
 
-public class TeamAnalyzer {
-    // All the "against" column suffixes:
-    static String[] types = {
-        "bug","dark","dragon","electric","fairy","fight",
-        "fire","flying","ghost","grass","ground","ice","normal",
-        "poison","psychic","rock","steel","water"
-    };
+# connection & cursor:
+connection = sqlite3.connect('../pokemon.sqlite')
+pokemonCursor = connection.cursor()
 
-    public static void main(String... args) throws Exception {
-        // Take six command-line parameters
-        if (args.length < 6) {
-            print("You must give me six Pokemon to analyze");
-            System.exit(-1);
-        }
+# All the "against" column suffixes:
+types = ["bug", "dark", "dragon", "electric", "fairy", "fight",
+    "fire", "flying", "ghost", "grass", "ground", "ice", "normal",
+    "poison", "psychic", "rock", "steel", "water"]
 
-        // This bit of JDBC magic I provide as a free gift :-)
-        // The rest is up to you.
-        try (Connection con = DriverManager.getConnection("jdbc:sqlite:pokemon.db")) {
-            for (String arg : args) {
-                print("Analyzing " + arg);
+# Take six parameters on the command-line
+if len(sys.argv) < 6:
+    print("You must give me six Pokemon to analyze!")
+    sys.exit()
 
-                // Analyze the pokemon whose pokedex_number is in "arg"
+team = []
+for i, arg in enumerate(sys.argv):
+    if i == 0:
+        continue
 
-                // You will need to write the SQL, extract the results, and compare
-                // Remember to look at those "against_NNN" column values; greater than 1
-                // means the Pokemon is strong against that type, and less than 1 means
-                // the Pokemon is weak against that type
-            }
+# Create list of against type.
+against = []
+for i in against:
+    final = pokemonCursor.execute("SELECT name, type1, type2, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water 
+    FROM imported_pokemon_data 
+    WHERE pokedex_number = {}".format(i))
 
-            String answer = input("Would you like to save this team? (Y)es or (N)o: ");
-            if (answer.equalsIgnoreCase("Y") || answer.equalsIgnoreCase("YES")) {
-                String teamName = input("Enter the team name: ");
+    # Create lists for pokemon, strong against, and weak against.
+    list_pokemon = list(cursor.fetchone())
+    against = list_pokemon[3:] # gets rid of name and type 1,2.
+    strong_against = []
+    weak_against = []
 
-                // Write the pokemon team to the "teams" table
-                print("Saving " + teamName + " ...");
-            }
-            else {
-                print("Bye for now!");
-            }
-        }        
-    }
+    print("Analyzing ", i)
+    # loop through every single i in the given length of the list.
+    for i in range(len(against)):
+        if against[i] < 1:
+            weak_against.append(types[i])
+        elif against[i] > 1:
+            strong_against.append(types[i])
+        else: continue
+    print("{} ({} {}) is strong against {} but weak against {}".format(list_pokemon[0], list_pokemon[1], list_pokemon[2], strong_against, weak_against))
 
-    /*
-     * These are here just to have some symmetry with the Python code
-     * and to make console I/O a little easier. In general in Java you
-     * would use the System.console() Console class more directly.
-     */
-    public static void print(String msg) {
-        System.console().writer().println(msg);
-    }
 
-    /*
-     * These are here just to have some symmetry with the Python code
-     * and to make console I/O a little easier. In general in Java you
-     * would use the System.console() Console class more directly.
-     */
-    public static String input(String msg) {
-        return System.console().readLine(msg);
-    }
-}
+    # Analyze the pokemon whose pokedex_number is in "arg"
+
+    # You will need to write the SQL, extract the results, and compare
+  
+# Ask to save the team or not.
+answer = input("Would you like to save this team? (Y)es or (N)o: ")
+if answer.upper() == "Y" or answer.upper() == "YES":
+    teamName = input("Enter the team name: ")
+
+    # Write the pokemon team to the "teams" table
+    print("Saving " + teamName + " ...")
+else:
+    print("Bye for now!")
+
--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -1,34 +1,51 @@
+# imports:
 import sqlite3  # This is the package for all sqlite3 access in Python
 import sys      # This helps with command-line parameters
 
+# connection & cursor:
+connection = sqlite3.connect('../pokemon.sqlite')
+pokemonCursor = connection.cursor()
+
 # All the "against" column suffixes:
-types = ["bug","dark","dragon","electric","fairy","fight",
-    "fire","flying","ghost","grass","ground","ice","normal",
-    "poison","psychic","rock","steel","water"]
+types = ["bug", "dark", "dragon", "electric", "fairy", "fight",
+   "fire", "flying", "ghost", "grass", "ground", "ice", "normal",
+   "poison", "psychic", "rock", "steel", "water"]
 
 # Take six parameters on the command-line
 if len(sys.argv) < 6:
-    print("You must give me six Pokemon to analyze!")
-    sys.exit()
+   print("You must give me six Pokemon to analyze!")
+   sys.exit()
+
 
 team = []
 for i, arg in enumerate(sys.argv):
-    if i == 0:
-        continue
+   if i == 0:
+       continue
 
-    # Analyze the pokemon whose pokedex_number is in "arg"
+   final = pokemonCursor.execute("SELECT name, type1, type2, against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water FROM imported_pokemon_data WHERE pokedex_number = {}".format(i))
 
-    # You will need to write the SQL, extract the results, and compare
-    # Remember to look at those "against_NNN" column values; greater than 1
-    # means the Pokemon is strong against that type, and less than 1 means
-    # the Pokemon is weak against that type
+   # Create lists for pokemon, strong against, and weak against.
+   list_pokemon = list(pokemonCursor.fetchone())
+   against = list_pokemon[3:] # gets rid of name and type 1,2.
+   strong_against = []
+   weak_against = []
 
+   print("Analyzing", i)
+   # loop through every single i in the given length of the list.
+   for i in range(len(against)):
+       if float(against[i]) < 1:
+           weak_against.append(types[i])
+       elif float(against[i]) > 1:
+           strong_against.append(types[i])
+       else: continue
+   print("{} ({} {}) is strong against {} but weak against {}".format(list_pokemon[0], list_pokemon[1], list_pokemon[2], strong_against, weak_against))
+
+# Ask to save the team or not.
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
-    teamName = input("Enter the team name: ")
+   teamName = input("Enter the team name: ")
 
-    # Write the pokemon team to the "teams" table
-    print("Saving " + teamName + " ...")
+# Write the pokemon team to the "teams" table
+   print("Saving " + teamName + " ...")
 else:
-    print("Bye for now!")
-
+   print("Bye for now!")
